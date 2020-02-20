5c57e781fa0db674f3f77e0ddcd54eafd59590b3
kristiinakeps <kristiina797@gmail.com>
2020-02-20 17:40:56 +0200

    first 500 sets on training data

--- files/1.ly
+++ files/1.ly
@@ -1,4 +1,4 @@
 \version "2.18.2"
 {
-f'
+e' a' a' d' f' c' e' f'
 }
--- lily.py
+++ lily.py
@@ -1,19 +1,19 @@
 import os
-import subprocess
-from PIL import Image
+from random import choices, randint
+
 from pdf2image import convert_from_path
-from random import choices
 
 
 def generate_lily_file(i):
     possible_notes = ['c\'', 'd\'', 'e\'', 'f\'', 'g\'', 'a\'', 'b\'', 'c\'\'']
     with open('files\{}.ly'.format(i), 'w') as file:
-        notes = choices(possible_notes)
+        notes = choices(possible_notes, k=randint(1, 8))
         file.write('\\version "2.18.2"\n{\n')
         file.write('{}\n'.format(' '.join(notes)))
         file.write('}')
 
-iterations = 5
+
+iterations = 500
 
 for i in range(iterations):
     generate_lily_file(i)
@@ -27,34 +27,10 @@ for i in range(iterations):
     for image in images:
         width, height = image.size
 
-        # Setting the points for cropped image
         left = 0
         top = 0
         right = width / 2
-        bottom = height / 6
+        bottom = height / 8
 
-        # Cropped image of above dimension
-        # (It will not change orginal image)
-        im1 = image.crop((left, top, right, bottom))
-        im1.save('files\{}.png'.format(i))
-        # image.save("proov.png")
-
-# Size of the image in pixels (size of orginal image)
-# # (This is not mandatory)
-# width, height = image.size
-#
-# # Setting the points for cropped image
-# left = 0
-# top = 0
-# right = width / 2
-# bottom = height / 6
-#
-# # Cropped image of above dimension
-# # (It will not change orginal image)
-# im1 = im.crop((left, top, right, bottom))
-# im1.save("new.png")
-
-
-# images = convert_from_path('test2.pdf', poppler_path=r"C:\Users\Kristiina\Downloads\poppler-0.68.0_x86\poppler-0.68.0\bin")
-# for image in images:
-#     image.save("proov.png")
+        cropped_image = image.crop((left, top, right, bottom))
+        cropped_image.save('files\{}.png'.format(i))
