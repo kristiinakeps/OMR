40c46b8978edf811016d73e63421fc1c526bd90b
kristiinakeps <kristiina797@gmail.com>
2020-03-15 20:06:57 +0200

    thresholds

--- recognition.py
+++ recognition.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
+from random import shuffle
 import cv2 as cv
 import numpy as np
 import os
-
 
 NOTE_LETTERS = ["c", "d", "e", "f", "g", "a", "b"]
 NOTE_LENGTHS = ["1", "2", "4"]
@@ -10,8 +10,8 @@ NOTE_OCTAVES = ["'"]
 
 # mallide lävendite jaoks on vaikeväärtus 0.8, osadele on määratud käsitsi
 THRES = defaultdict(lambda: 0.8)
-THRES_VALUES = {"c'1": 0.7, "c'2": 0.72, "c'4": 0.85,
-                "d'1": 0.75, "d'2": 0.74, "d'4": 0.87,
+THRES_VALUES = {"c'1": 0.7, "c'2": 0.72, "c'4": 0.83,
+                "d'1": 0.75, "d'2": 0.74, "d'4": 0.8,
                 "e'2": 0.77, "e'4": 0.87,
                 "f'2": 0.77, "f'4": 0.9,
                 "g'1": 0.75, "g'2": 0.75, "g'4": 0.85,
@@ -23,12 +23,22 @@ for k, v in THRES_VALUES.items():
 
 PRIORITY = ["c'4", "e'4", "d'4", "g'4"]
 PRIORITY_PAIRS = [("c'1", "e'1"), ("c'2", "c'4"), ("c'2", "e'2"), ("c'2", "e'2"), ("c'4", "e'2"), ("c'4", "e'4"),
-                  ("d'2", "d'4"),
+                  ("d'2", "d'4"), ("d'4", "f'4"),
                   ("e'2", "e'4"),
-                  ("f'2", "f'4"), ("f'4", "d'4"),
+                  ("f'2", "f'4"),
                   ("g'2", "c'2"), ("g'2", "g'4"), ("g'4", "e'4"),
                   ("a'4", "a'2")
                   ]
+
+COLORS = [(220, 20, 60), (255, 0, 0), (205, 92, 92), (240, 128, 128), (255, 69, 0), (255, 165, 0), (255, 215, 0),
+          (154, 205, 50),
+          (107, 142, 35), (124, 252, 0), (0, 100, 0), (50, 205, 50), (0, 255, 127), (32, 178, 170), (0, 128, 128),
+          (0, 255, 255),
+          (127, 255, 212), (100, 149, 237), (0, 191, 255), (30, 144, 255), (25, 25, 112), (65, 105, 225), (75, 0, 130),
+          (138, 43, 226), (72, 61, 139), (147, 112, 219), (148, 0, 211), (186, 85, 211), (128, 0, 128), (238, 130, 238),
+          (255, 105, 180),
+          (244, 164, 96), (139, 69, 19), (188, 143, 143), (176, 196, 222)
+          ]
 
 
 def get_locations(path, note, threshold):  # tähe esinemised pildil
@@ -80,8 +90,8 @@ def clean_result(result):
                 choice = third
         try:
             # kui puhastatud järjendis viimane noot on liiga lähedal hetkel valitule, siis seda ei lisa
-            if new_result[len(new_result) - 1] == choice or (abs( new_result[len(new_result) - 1][0]- choice[0]) < 30) \
-                    and (abs( new_result[len(new_result) - 1][1] - choice[1]) < 30):
+            if new_result[len(new_result) - 1] == choice or (abs(new_result[len(new_result) - 1][0] - choice[0]) < 30) \
+                    and (abs(new_result[len(new_result) - 1][1] - choice[1]) < 30):
                 pass
             else:
                 new_result.append(choice)
@@ -116,17 +126,30 @@ def lyrics_from_file(filename):
     rows = [[]]
     y = 0  # hoiame meeles eelmise y-koordnaadi
     for res in result:
-        if abs(res[1] - y) > 50:  # kui uue elemendi y-koordinaat erineb rohkem kui 50 võrra, siis eeldame et alustasime uue reaga
+        if abs(res[
+                   1] - y) > 50:  # kui uue elemendi y-koordinaat erineb rohkem kui 50 võrra, siis eeldame et alustasime uue reaga
             rows.append([])  # iga rea jaoks uus järjend
         rows[-1].append(res)  # lisame viimasesse ritta
         y = res[1]
     rows = [sorted(row, key=lambda el: el[0]) for row in rows]
     flatten_rows = [el for row in rows for el in row]
     result = clean_result(flatten_rows)
+    create_image_with_rectangles(filename, result)
     output = [el[2] for el in result]
 
     return output
 
+def create_image_with_rectangles(image_path, locations):
+    img = cv.imread(image_path)
+    w, h = 50, 130
+    shuffle(COLORS)
+    notes = [letter + octave + length for letter in NOTE_LETTERS for octave in NOTE_OCTAVES for length in NOTE_LENGTHS]
+    colors = dict(zip(notes, COLORS[:len(notes)]))
+    for x, y, note in locations:
+        # rectangle(pilt, algusnurk (koordinaadid), lõppnurk, värv (BGR), joone paksus)
+        cv.rectangle(img, (x, y), (x + w, y+ h), colors[note], 2)
+    cv.imwrite("testing\\rectangles\\result.png", img)
+
 
 if __name__ == "__main__":
-    print(lyrics_from_file("testing\\test.png"))
+    print(lyrics_from_file("testing\\mary_lamb.png"))
