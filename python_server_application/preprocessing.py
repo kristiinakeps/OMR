76ec70947c9b74f1482a1bfb67d954ec9d107800
kristiinakeps <kristiina797@gmail.com>
2020-04-26 12:02:07 +0300

    fix

--- python_server_application/preprocessing.py
+++ python_server_application/preprocessing.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import staff
+import staffs
 
 TEMPLATE_STAFF_SPACE = 14
 BLUR_KERNEL = 3
@@ -17,30 +17,11 @@ def preprocessed_data(image_bytes):
     thres, img_black_and_white = cv.threshold(img_gray, 127, 255, cv.THRESH_BINARY)
     cv.imwrite('../testing/tulemus.png', img_black_and_white)
 
-    staffs = staff.staff_detection(img_gray)
-    if len(staffs) > 1:
-        space_between_staffs = staff.find_space_between_lines(staffs)
+    staff_lines = staffs.staff_detection(img_gray)
+    if len(staff_lines) > 1:
+        space_between_staffs = staffs.find_space_between_lines(staff_lines)
         width, height = img_black_and_white.shape
         size_difference = TEMPLATE_STAFF_SPACE / space_between_staffs
         img_sized = cv.resize(img_black_and_white, (int(height * size_difference), int(width * size_difference)))
-        return img_sized, staffs, space_between_staffs, size_difference
+        return img_sized, staff_lines, space_between_staffs, size_difference
 
-
-if __name__ == "__main__":
-    with open("../testing/camera.jpg", "rb") as f:
-        # with open("../testing/mary_lamb.png", "rb") as f:
-        # with open("../testing/prediction.png", "rb") as f:
-        # with open("../testing/test.png", "rb") as f:
-        # with open("../testing/test_rythm.png", "rb") as f:
-        # with open("../testing/high.png", "rb") as f:
-        image_bytes = f.read()
-    res = preprocessed_data(image_bytes)
-    if res is not None:
-        import recognition
-
-        result, staffs, space, size_difference = res
-        clef = recognition.get_clef(result)
-        is_treble = True if clef == 'treble' else False
-        coordinates_and_notes, rows = staff.group_and_identify(staffs, space, size_difference, is_treble)
-        locs = recognition.recognize_all_symbols(result, coordinates_and_notes, rows)
-        print(locs)
